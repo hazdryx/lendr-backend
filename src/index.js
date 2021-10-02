e02827021293cb58f750227c94608493f964e734
Hazdryx <hazdryx@gmail.com>
2021-10-02 14:21:23 -0500

    Improved API structure and security.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
 app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.set('trust proxy', 1);
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: {
+        'err': 'Too many requests.'
+    }
+});
+app.use(limiter);
+
 app.use('/api', require('./api'));
+
+// 404 Fallback.
+app.use((req, res) => {
+    res.status(404).send({ err: `Cannot ${req.method.toUpperCase()} ${req.url}` });
+});
 
 // Connect to database.
 mongoose.connect(process.env.DB_HOST, {
